bb657625a3ca6b9db71e717da7109e3f8c54bec4
dieulinh12 <67354349+dieulinh12@users.noreply.github.com>
2020-07-22 01:13:53 +0700

    baitap

--- HOMEWORK_04/baitap2/jquery.js
+++ HOMEWORK_04/baitap2/jquery.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
     $("#select").change(function(){
-        var sq_num=$(this).val();
+        var sq_num=$('this').val();
         $(".sq_b").html("");
-        for(let i=0; i<sq_num; i++){
-            $(".sq_b").append('<div class')
-        }
+        /*for(let i=0; i<sq_num; i++){
+            $(".sq_b").append('.square')
+            /*$('.div').addClass(".square")*/
+        if(sq_num=="1")
+          {
+              $('.sq_b').addclass(".square")
+          }
     });
 });
